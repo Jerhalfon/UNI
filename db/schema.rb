021c71a7c9fd94b5f57ccfe2baeb20cc58cbd348
Jean Van Marcke <jean.vmdl@gmail.com>
2019-11-12 11:47:10 +0100

    all categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_160512) do
+ActiveRecord::Schema.define(version: 2019_11_12_092746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_160512) do
     t.string "application_deadline"
     t.string "student_profile"
     t.string "admission_requirements"
+    t.string "language"
     t.index ["category_id"], name: "index_programs_on_category_id"
     t.index ["university_id"], name: "index_programs_on_university_id"
   end
