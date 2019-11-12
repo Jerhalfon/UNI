5389826b1c5a65ba5b306f7c0c531e9e9169776f
Jeremy Halfon <jerhalfon@hotmail.com>
2019-11-12 17:32:57 +0100

    search avec duration level categroy...

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -4,13 +4,19 @@ class ProgramsController < ApplicationController
   before_action :set_program, only: [:show]
 
   def index
-    name = params[:program][:name]
-    price = params[:price] == "" ? [1, 1_000_000_000] : [params[:price]]
-    uni = params[:program][:university_id] == "" ? [1, 1_000_000_000] : [params[:program][:university_id]]
 
-    @programs = Program.search where: { name: /.*#{name}.*/, university_id: { all: uni }, price: { all: price } }
-    raise
-    # , price: price
+    name = params[:program][:name]
+    price = params[:price].to_i.zero? ? (0..1000000000) : (0..params[:price].to_i)
+    location_id = params[:program][:university][:location_id]
+    category = params[:program][:category_id] == "" ? (0..100).to_a : (params[:program][:category_id].to_i)
+    duration = params[:program][:duration] == "" ? (0..150).to_a : (0..delete_month(params[:program][:duration])).to_a
+    level = params[:program][:duration] == "" ? ["Bachelor", "Master"] : [params[:program][:duration]]
+raise
+    @programs = Program.search where: { name: /.*#{name}.*/, price: price, category_id: category, duration: duration, level: level }
+
+    unless location_id == ""
+      @programs = @programs.select { |program| program.university.location.id == location_id.to_i }
+    end
   end
 
   def show
@@ -23,5 +29,9 @@ class ProgramsController < ApplicationController
 
   def set_program
     @program = Program.find(params[:id])
+  end
+
+  def delete_month(choice)
+    choice.split(' ').first.to_i
   end
 end
