c3519b6f48a51129b45de67326997767b275ee15
Jeremy Halfon <jerhalfon@hotmail.com>
2019-11-18 17:48:48 +0100

    beginning of chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,18 @@
 import "bootstrap";
 import Sortable from 'sortablejs';
+import {sliderValue} from "../components/slider";
+import {chart from} "../components/personality_stat";
 
-Sortable.create(sortableCard, {
-  handle: '.card-favorite',
-  animation: 150
-});
+sliderValue();
+chart.render();
+
+const sortableCard = document.getElementById("sortableCard");
+if (sortableCard) {
+  Sortable.create(sortableCard, {
+    handle: '.card-favorite',
+    animation: 150
+  });
+}
+
+
+
