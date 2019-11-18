9bf293d2661acd728f8f19f5bc3a38b951d138d0
Romain Vriamont <rom_vriamont@hotmail.com>
2019-11-18 16:35:07 +0100

    avatar review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_134507) do
+ActiveRecord::Schema.define(version: 2019_11_18_144458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_134507) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "contact"
     t.index ["program_id"], name: "index_reviews_on_program_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
@@ -112,6 +113,9 @@ ActiveRecord::Schema.define(version: 2019_11_14_134507) do
     t.bigint "location_id"
     t.string "photo"
     t.string "remote_photo_url"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["location_id"], name: "index_universities_on_location_id"
   end
 
